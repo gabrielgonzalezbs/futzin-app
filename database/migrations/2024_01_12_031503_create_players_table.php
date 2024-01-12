4b5817adc1982059947eab12e9f04ab78063781d
gabrielgonzalez <gabriel.gonzalezbs@gmail.com>
2024-01-12 00:24:25 -0300

    FEAT: Criação do model e migrations dos jogadores

---
+++ database/migrations/2024_01_12_031503_create_players_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('email', 255);
+            $table->double('skills_levels', 8, 2);
+            $table->boolean('goalkeeper');
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
