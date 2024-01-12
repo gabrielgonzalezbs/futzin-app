edf2d3ad0d0528ec8e11275bbba4a5139f7d447a
gabrielgonzalez <gabriel.gonzalezbs@gmail.com>
2024-01-12 00:38:59 -0300

    FEAT: Criação do model e migrations das partidas e os jogadores que irão compor aquela partida

---
+++ database/migrations/2024_01_12_033219_create_players_matches_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players_matches', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('confirmed');
+            $table->dateTime('confirmation_date');
+            $table->string('team_name', 255);
+            $table->foreignId('player_id')
+                ->references('id')
+                ->on('players')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('matche_id')
+                ->references('id')
+                ->on('matches')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players_matches');
+    }
+};
