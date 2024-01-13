8bd388464cb65e9c2ade7315089d5fce83809bab
gabrielgonzalez <gabriel.gonzalezbs@gmail.com>
2024-01-13 18:04:19 -0300

    FEAT: Adicionadas rotas para o CRUD de jogadores

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PlayersController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,10 +23,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/players', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('player');
-
 Route::get('/matches', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('matche');
@@ -34,6 +31,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/players', PlayersController::class)
+        ->except(['show']);
+
 });
 
 require __DIR__.'/auth.php';
