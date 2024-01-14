2962cf01b79ee7622383be6ca8438e781029d009
gabrielgonzalez <gabriel.gonzalezbs@gmail.com>
2024-01-14 03:45:42 -0300

    FEAT: Adicionado join com as tabela de jogadores e a tabela de partidas

--- app/Models/PlayersMatches.php
+++ app/Models/PlayersMatches.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PlayersMatches extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'player_id',
+        'matche_id',
+        'confirmed',
+        'confirmation_date',
+        'team_name',
+    ];
+
+    /**
+     * Get the players that owns the matche.
+     */
+    public function players(): BelongsTo
+    {
+        return $this->belongsTo(Players::class, 'player_id');
+    }
+
+        /**
+     * Get the players that owns the matche.
+     */
+    public function matches(): BelongsTo
+    {
+        return $this->belongsTo(Matches::class, 'matche_id');
+    }
 }
